48999549cc17effd5ab0d226438ecd220f6a23f6
Olalekan Akande <35519851-aoakande2016@users.noreply.replit.com>
2024-08-29 14:17:36 +0000

    Adding a UI for my Decentralised voting system

---
+++ client/src/components/VotingSystem.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import { useConnect } from '@stacks/connect-react';
+import { fetchProposals } from './votingApi';
+import ProposalList from './ProposalList';
+import CreateProposal from './CreateProposal';
+
+const VotingSystem = () => {
+  const { doContractCall } = useConnect();
+  const [proposals, setProposals] = useState([]);
+
+  useEffect(() => {
+    const getProposals = async () => {
+      const fetchedProposals = await fetchProposals();
+      setProposals(fetchedProposals);
+    };
+    getProposals();
+  }, []);
+
+  return (
+    <div>
+      <h1>Decentralized Voting System</h1>
+      <CreateProposal doContractCall={doContractCall} onProposalCreated={() => fetchProposals()} />
+      <ProposalList proposals={proposals} doContractCall={doContractCall} onVoteOrEnd={() => fetchProposals()} />
+    </div>
+  );
+};
+
+export default VotingSystem;
